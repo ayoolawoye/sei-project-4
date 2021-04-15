faddf544be697878990998a667f655f35cb5d63e
Ayo Olawoye <ayo.olawoye@gmail.com>
2021-04-15 18:02:10 +0100

    django url errors

--- project-four/project-django-app/skills/models.py
+++ project-four/project-django-app/skills/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Skill(models.Model):
-    skill_name = models.CharField(max_length=50)
-    skill_price = models.CharField(max_length=50)
-    skill_image = models.CharField(max_length=300)
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    image = models.CharField(max_length=300)
 
     def __str__(self):
-        return f"{self.skill_name} - {self.skill_price}"
+        return f"{self.name} - {self.price}"
--- project-four/project-django-app/skills/views.py
+++ project-four/project-django-app/skills/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Skill
+from .serializers.common import SkillSerializer
+
+
+class SkillListView(APIView):
+    def get(self, _request):
+        skills = Skill.objects.all()
+        serialized_skills = SkillSerializer(skills, many=True)
+        return Response(serialized_skills.data, status=status.HTTP_200_OK)
